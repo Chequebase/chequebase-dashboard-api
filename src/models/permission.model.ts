d647504fe63f015719353f5ef4e3550f3e126a36
Uzochukwu Onuegbu <uzochukwu.onuegbu25@gmail.com>
2023-12-06 14:48:47 +0100

    permission model

--- src/models/permission.model.ts
+++ src/models/permission.model.ts
@@ -1,9 +1,14 @@
 import { cdb } from '@/modules/common/mongoose';
-import { Schema } from 'mongoose';
+import { Schema, Types } from 'mongoose';
 
 export interface IPermission {
+  _id: Types.ObjectId
   name: string
   description: string
+  permissions: {
+    name: string
+    constraints: string[]
+  }[]
   createdAt: Date
   updatedAt: Date
 }
@@ -12,6 +17,15 @@ const permissionSchema = new Schema<IPermission>(
   {
     name: { type: String, required: true },
     description: { type: String, required: true },
+    permissions: {
+      _id: false,
+      type: [{
+        name: String,
+        constraints: {
+            type: [String]
+        }
+      }]
+    }
   },
   { timestamps: true },
 );
