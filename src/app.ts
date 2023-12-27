e0133530d8088f259989c3321c603afa839e0d6f
Uzochukwu Onuegbu <uzochukwu.onuegbu25@gmail.com>
2023-12-27 13:33:35 +0100

    increase file limit

--- src/app.ts
+++ src/app.ts
@@ -29,11 +29,10 @@ app.set("trust proxy", true);
 app.use(helmet());
 app.use(cors());
 // ?NOTE: make sure to use @JsonController()
-// app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
 app.use(apiRequestLogger)
-// app.use(express.urlencoded({ extended: true }));
-app.use(bodyParser.json({ limit: '50mb' }));
-app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+app.use(express.urlencoded({ extended: true }));
+// app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
 
 app.get("/health", (_: Request, res: Response) => {
