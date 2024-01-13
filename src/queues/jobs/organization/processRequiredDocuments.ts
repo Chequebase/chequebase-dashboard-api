439843f0e2d3705be64b560a8edf6ac14f5c578e
Uzochukwu Onuegbu <uzochukwu.onuegbu25@gmail.com>
2024-01-13 16:23:15 +0100

    chahes

--- src/queues/jobs/organization/processRequiredDocuments.ts
+++ src/queues/jobs/organization/processRequiredDocuments.ts
@@ -5,10 +5,10 @@ import VirtualAccount from "@/models/virtual-account.model";
 import WalletEntry, { WalletEntryScope, WalletEntryStatus, WalletEntryType } from "@/models/wallet-entry.model";
 import Wallet, { IWallet } from "@/models/wallet.model";
 import Logger from "@/modules/common/utils/logger";
-import { BadRequestError } from "routing-controllers";
+import { BadRequestError, NotFoundError } from "routing-controllers";
 import { cdb } from "@/modules/common/mongoose";
 import { formatMoney, transactionOpts } from "@/modules/common/utils";
-import { IOrganization } from "@/models/organization.model";
+import Organization, { IOrganization } from "@/models/organization.model";
 import { IUser } from "@/models/user.model";
 
 export interface KYCProviderData {
@@ -25,7 +25,13 @@ const logger = new Logger('process-required-documents.job')
 
 async function processRequiredDocuments(job: Job<RequiredDocumentsJobData>) {
     const data = job.data
-    console.log({ data })
+
+    const organization = await Organization.findOne({
+        anchorCustomerId: data.customerId,
+      }).lean()
+    if (!organization) throw new NotFoundError('Organization not found')
+
+    await Organization.updateOne({ _id: organization._id }, { anchor: { ...organization.anchor, requiredDocuments: data.requiredDocuments }})
     return data
 }
 
