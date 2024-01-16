a8649817c2471f5374ce7262859be1d2847ece6f
Uzochukwu Onuegbu <uzochukwu.onuegbu25@gmail.com>
2024-01-16 11:13:31 +0100

    WIP: upload to anchor

--- src/modules/banksphere/providers/anchor.client.ts
+++ src/modules/banksphere/providers/anchor.client.ts
@@ -44,7 +44,7 @@ export class AnchorCustomerClient implements CustomerClient {
   public async uploadCustomerDocuments(payload: UploadCustomerDocuments) {
     try {
       const formData = new FormData()
-      formData.append('fileData', new Blob([payload.fileData]));
+      formData.append('fileData', new Blob(payload.fileData));
       console.log({ headers: formData.getHeaders() })
       // this.http.defaults.headers.common['Content-Type'] = 'multipart/form-data'
       const res = await this.http.post(`/api/v1/documents/upload-document/${payload.customerId}/${payload.documentId}`, formData, {
