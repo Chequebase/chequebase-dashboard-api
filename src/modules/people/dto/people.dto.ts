03a4307819b452660a557d5d703879d4aa8232ff
esiro <daviesesiro@gmail.com>
2024-03-08 00:56:54 +0100

    feat: handle bulk user invite

--- src/modules/people/dto/people.dto.ts
+++ src/modules/people/dto/people.dto.ts
@@ -1,5 +1,5 @@
 import { Type } from "class-transformer";
-import { ArrayMinSize, IsArray, IsString, ValidateNested } from "class-validator";
+import { ArrayMinSize, IsArray, IsEmail, IsOptional, IsString, ValidateNested } from "class-validator";
 
 export class CreateDepartmentDto {
   @IsString()
@@ -19,4 +19,36 @@ export class CreateDepartmentDto {
   @ValidateNested({ each: true })
   @ArrayMinSize(1)
   budgets: string[]
+}
+
+export class SendMemberInviteDto {
+  @IsArray()
+  @Type(() => InviteData)
+  @ValidateNested({ each: true })
+  @ArrayMinSize(1)
+  invites: InviteData[]
+}
+
+class InviteData {
+  @IsString()
+  name: string
+
+  @IsString()
+  role: string
+
+  @IsString()
+  @IsEmail()
+  email: string
+
+  @IsString()
+  @IsOptional()
+  phoneNumber: string
+
+  @IsString()
+  @IsOptional()
+  manager: string
+
+  @IsString()
+  @IsOptional()
+  department: string
 }
