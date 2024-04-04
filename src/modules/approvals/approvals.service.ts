8173d840070b74194378b0df4d00cf5713a3d960
esiro <daviesesiro@gmail.com>
2024-04-04 02:47:50 +0100

    fix: approve request

--- src/modules/approvals/approvals.service.ts
+++ src/modules/approvals/approvals.service.ts
@@ -129,7 +129,7 @@ export default class ApprovalService {
     let request = await ApprovalRequest.findOne({
       _id: requestId,
       organization: auth.orgId,
-      'reviews.user': auth.orgId
+      'reviews.user': auth.userId
     })
 
     if (!request) {
