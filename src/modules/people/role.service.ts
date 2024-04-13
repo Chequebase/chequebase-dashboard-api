6486182670bb7fe113605a6112041ff9592aed34
esiro <daviesesiro@gmail.com>
2024-04-14 00:46:13 +0100

    feat: sort roles by rank

--- src/modules/people/role.service.ts
+++ src/modules/people/role.service.ts
@@ -15,6 +15,7 @@ export class RoleService {
       ]
     })
       .populate({ path: 'permissions', select: 'name module actions' })
+      .sort('rank')
       .lean()
     
     const populatedRoles = await Promise.all(roles.map(async r => {
