39268015ac213243e490dee264c8e190f832453c
esiro <daviesesiro@gmail.com>
2024-04-21 16:13:50 +0100

    refactor: rename pageSize to limit

--- src/modules/approvals/approvals.service.ts
+++ src/modules/approvals/approvals.service.ts
@@ -104,7 +104,7 @@ export default class ApprovalService {
 
     const requests = await ApprovalRequest.paginate(filter.object, {
       page: Number(query.page),
-      limit: query.pageSize,
+      limit: query.limit,
       sort: '-createdAt',
       populate: [
         { path: 'approvalRule', select: 'approvalType workflowType' },
--- src/modules/approvals/dto/approvals.dto.ts
+++ src/modules/approvals/dto/approvals.dto.ts
@@ -64,7 +64,7 @@ export class GetApprovalRequestsQuery {
   page: number
 
   @IsInt()
-  pageSize: number
+  limit: number
 
   @IsBoolean()
   reviewed: boolean
