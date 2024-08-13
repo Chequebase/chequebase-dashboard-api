78668b77a1978a682559f08598568836a5cd6ed7
Uzochukwu Onuegbu <uzochukwu.onuegbu25@gmail.com>
2024-08-13 19:06:00 +0200

    added transfer fee script

---
+++ src/scripts/2-calculate-transfer-fee-revenue.ts
@@ -0,0 +1,18 @@
+import 'module-alias/register'
+process.env.DB_URI = 'mongodb+srv://chequebase:XEamP9X0FpVeDggv@cluster0.cx1ni.mongodb.net/chequebase-staging?retryWrites=true&w=majority'
+// process.env.DB_URI = 'mongodb+srv://chequebase:vhe3eOqqCol7YFWn@chequebase-prod.9t1nwt8.mongodb.net/chequebase?retryWrites=true&w=majority'
+import WalletEntry from '@/models/wallet-entry.model';
+
+async function run() {
+        const walletEntries = await WalletEntry.find({ scope: 'budget_transfer' })
+          .select('fee')
+
+        const totalFeeInKobo = walletEntries.reduce((total, entry) => total + entry.fee, 0);
+        const totalFeeInNaira = Math.floor(totalFeeInKobo / 100);
+
+        const txNo = walletEntries.length;
+        const totalAcnhorFee = txNo * 20
+        console.log({ totalFeeInNaira, txs: walletEntries.length, totalAcnhorFee, ourOwnMoney: totalFeeInNaira - totalAcnhorFee })
+}
+
+run()
