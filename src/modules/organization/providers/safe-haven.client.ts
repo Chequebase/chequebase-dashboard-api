6cb50565a59e44ba006babb4f639f6f6221bf6b1
Uzochukwu Onuegbu <uzochukwu.onuegbu25@gmail.com>
2024-12-01 21:21:36 +0100

    changes

--- src/modules/organization/providers/safe-haven.client.ts
+++ src/modules/organization/providers/safe-haven.client.ts
@@ -58,6 +58,50 @@ export class SafeHavenIdentityClient {
       };
     }
   }
+
+  async initiateCACVerification(
+    cac: string
+  ){
+    const body = {
+      type: 'CAC',
+      number: cac,
+      debitAccountNumber: settlementAccount,
+      async: false
+    };
+
+    try {
+      const { data, status } = await this.httpClient.axios.post(
+        "/identity/",
+        body
+      );
+      const success = data?.data?.status === "SUCCESS";
+
+      this.logger.log("safe-haven initiate bvn check response", {
+        payload: cac,
+        response: JSON.stringify(data),
+        status,
+      });
+      return {
+        status: success ? "successful" : "failed",
+        identityId: data.data._id,
+        gatewayResponse: JSON.stringify(data),
+      };
+    } catch (err: any) {
+      this.logger.error("error processing bvn check", {
+        reason: JSON.stringify(err),
+        payload: JSON.stringify(cac),
+        requestData: JSON.stringify(body),
+        status: err.response?.status,
+      });
+
+      return {
+        status: "failed",
+        message:
+          "Unable to process bvn check",
+        gatewayResponse: JSON.stringify(err),
+      };
+    }
+  }
 //   type response
   async validateVerification(identityId: string, otp: string): Promise<any> {
     try {
@@ -94,3 +138,49 @@ export class SafeHavenIdentityClient {
     }
   }
 }
+
+// async function run() {
+//   const vaClient = Container.get<SafeHavenIdentityClient>(SAFE_HAVEN_IDENTITY_TOKEN)
+//   try {
+//     const account = await vaClient.initiateCACVerification('196011');
+//     console.log({ account })
+//     // const providerRef = account.providerRef || accountRef
+//     // const wallet = await Wallet.create({
+//     //   _id: walletId,
+//     //   organization: '66e2cd42bb0baa2b6d513349',
+//     //   baseWallet: baseWallet,
+//     //   currency: 'NGN',
+//     //   balance: 0,
+//     //   primary: true,
+//     //   virtualAccounts: [virtualAccountId]
+//     // })
+
+//     // const virtualAccount = await VirtualAccount.create({
+//     //   _id: virtualAccountId,
+//     //   organization: '66e2cd42bb0baa2b6d513349',
+//     //   wallet: wallet._id,
+//     //   accountNumber: account.accountNumber,
+//     //   bankCode: account.bankCode,
+//     //   name: account.accountName,
+//     //   bankName: account.bankName,
+//     //   provider,
+//     //   externalRef: providerRef,
+//     // });
+
+//     // console.log({
+//     //   _id: wallet._id,
+//     //   balance: wallet.balance,
+//     //   currency: wallet.currency,
+//     //   account: {
+//     //     name: virtualAccount.name,
+//     //     accountNumber: virtualAccount.accountNumber,
+//     //     bankName: virtualAccount.bankName,
+//     //     bankCode: virtualAccount.bankCode
+//     //   }
+//     // })
+// } catch (error) {
+//     console.log({ error })
+//   }
+// }
+
+// run()
