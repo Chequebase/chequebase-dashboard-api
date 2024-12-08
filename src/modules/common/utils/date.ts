dc54e20d149a58ea4824cc305abb9151c078f191
esiro <daviesesiro@gmail.com>
2024-12-08 13:37:33 +0100

    fix: get dates util

--- src/modules/common/utils/date.ts
+++ src/modules/common/utils/date.ts
@@ -1,25 +1,34 @@
 import dayjs from "dayjs"
+import timezone from "dayjs/plugin/timezone";
+import utc from "dayjs/plugin/utc";
+
+dayjs.extend(utc);
+dayjs.extend(timezone);
+
+const tz = "Africa/Lagos";
+dayjs.tz.setDefault(tz);
+
 
 export function getPrevFromAndTo(fromStr: string, toStr: string) {
-  const from = dayjs(fromStr, { utc: true }).startOf('day').toDate()
-  const to = dayjs(toStr, { utc: true }).endOf('day').toDate()
+  const from = dayjs(fromStr).tz(tz, true).startOf('day')
+  const to = dayjs(toStr).tz(tz, true).endOf("day")
 
-  const daysDiff = dayjs(toStr, { utc: true }).diff(fromStr, 'days')
+  const daysDiff = to.diff(from, 'days')
   const subtractDays = daysDiff > 0 ? daysDiff : 1
 
-  const prevTo = dayjs(fromStr, { utc: true })
+  const prevTo = from
     .subtract(1, 'day')
     .endOf('day')
     .toDate()
-  const prevFrom = dayjs(from).subtract(subtractDays, 'days').toDate()
+  const prevFrom = from.subtract(subtractDays, 'days').toDate()
 
-  return { from, to, prevTo, prevFrom }
+  return { from: from.toDate(), to: to.toDate(), prevTo, prevFrom }
 }
 
 export function getDates(from: string | Date, to: string | Date, period: string = 'M') {
   const dates: { to: Date; from: Date }[] = []
-  let start = dayjs(from, { utc: true }).startOf('day')
-  const end = dayjs(to, { utc: true }).endOf('day')
+  let start = dayjs(from).tz(tz, true).startOf('day')
+  const end = dayjs(to).tz(tz, true).endOf("day");
 
   while (dayjs(dates[dates.length - 1]?.to || start).isBefore(end)) {
     dates.push({
