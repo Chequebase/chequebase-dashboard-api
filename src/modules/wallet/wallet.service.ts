9896e2993786fd75a38292748286e57f074c582a
chequebase-dev <uyochukwo@MacBook-Pro-3.fritz.box>
2025-01-30 17:34:40 +0100

    return

--- src/modules/wallet/wallet.service.ts
+++ src/modules/wallet/wallet.service.ts
@@ -495,6 +495,7 @@ export default class WalletService {
           WalletEntryScope.WalletTransfer,
           WalletEntryScope.BudgetFunding,
           WalletEntryScope.PayrollFunding,
+          WalletEntryScope.PayrollWithdraw
         ]
       })
     }
