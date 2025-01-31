81c79c24de783b65fb5527a14cbda8de7cab2742
daviesesiro <daviesesiro@gmail.com>
2025-01-31 22:08:34 +0100

    refactor: only show completed payroll on payroll statistics

--- src/modules/payroll/payroll.service.ts
+++ src/modules/payroll/payroll.service.ts
@@ -278,6 +278,7 @@ export class PayrollService {
     const result = await Payroll.aggregate()
       .match({
         organization: new ObjectId(orgId),
+        status: PayrollStatus.Completed,
         date: {
           $gte: today.startOf("year").toDate(),
           $lte: today.endOf("year").toDate(),
